1c0f7f51077d73cfa253e90756f8a71707656f26
Nikolay <Nikolay@Nikolay-W510>
2011-05-19 20:38:19 -0700

    code formatting and conversion to scala.App

--- src/ApproxValue.scala
+++ src/ApproxValue.scala
@@ -1,21 +1,20 @@
-object ApproxValue {
-implicit def double2ApproxValue(d : Double) = new ApproxValue(d)
-class ApproxValue(value : Double) {
-def ~(other : Double) = {
-math.abs(value - other);
-}
-def ± (v:Double)={
-var b = false;
-if (value<v)
-b = true;
-b;
-}
-}
-def main(args: Array[String]){
-val a = 2.0
-val b = math.sqrt(2)
-val c = b * b
-println(a == c) // prints false
-println(a ~ c ± 1E-12) // should print true because Math.abs(a - c) < 1E-12
-}
+object ApproxValue extends App {
+  implicit def double2ApproxValue(d: Double) = new ApproxValue(d)
+  class ApproxValue(value: Double) {
+    def ~(other: Double) = {
+      math.abs(value - other);
+    }
+    def ±(v: Double) = {
+      var b = false;
+      if (value < v)
+        b = true;
+      b;
+    }
+  }
+
+  val a = 2.0
+  val b = math.sqrt(2)
+  val c = b * b
+  println(a == c) // prints false
+  println(a ~ c ± 1E-12) // should print true because Math.abs(a - c) < 1E-12
 }
